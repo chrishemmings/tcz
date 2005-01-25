0a9650791a9f91fd4df8ca127b10a79a90f5d3bc
tcz_monster <>
2005-01-25 19:22:02 +0000

    Wrong email address, Jon :-)

--- include/modules_authors.h
+++ include/modules_authors.h
@@ -69,7 +69,7 @@ struct author_details authors[] = {
        {NULL, NULL, NULL, 0, 0,
 	  "RH",  "Robin Heydon",       NULL,        NULL},
        {NULL, NULL, NULL, 0, 0,
-	  "SM",  "Shaun McVey",        "Monster",   "tcz_monster@users.sourceforge.net"},
+	  "SM",  "Shaun McVey",        "Monster",   "shaun.mcvey@gmail.com"},
        {NULL, NULL, NULL, 0, 0,
 	  "SAB", "Simon Boggis",       "Badpenny",  NULL},
        {NULL, NULL, NULL, 0, 0,
